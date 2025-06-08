151bb16c80db567208a7d34c98c956cac38537ed
James Anderson <joji@ando.ws>
2025-06-08 21:33:20 +0100

    Adjust inset slightly

--- src/main/java/yungando/removeinvisibleitemframeinset/mixin/ItemFrameEntityRendererMixin.java
+++ src/main/java/yungando/removeinvisibleitemframeinset/mixin/ItemFrameEntityRendererMixin.java
@@ -10,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ItemFrameEntityRendererMixin {
 	@Redirect(method = "render(Lnet/minecraft/entity/decoration/ItemFrameEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(FFF)V", ordinal = 1))
 	private void disableInvisibleItemFrameOffset(MatrixStack matrixStack, float x, float y, float z) {
-		matrixStack.translate(x, y, 0.475F);
+		matrixStack.translate(x, y, 0.45F);
 	}
 }
