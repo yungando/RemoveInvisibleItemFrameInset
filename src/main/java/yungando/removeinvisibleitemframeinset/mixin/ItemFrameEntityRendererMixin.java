304066bbfe2e1c2afb6cae1bd6eee0070bbc8c93
James Anderson <joji@ando.ws>
2025-06-09 19:11:02 +0100

    Remove invisible item frame label

--- src/main/java/yungando/removeinvisibleitemframeinset/mixin/ItemFrameEntityRendererMixin.java
+++ src/main/java/yungando/removeinvisibleitemframeinset/mixin/ItemFrameEntityRendererMixin.java
@@ -1,15 +1,31 @@
 package yungando.removeinvisibleitemframeinset.mixin;
 
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemFrameEntityRenderer.class)
-public class ItemFrameEntityRendererMixin {
+public abstract class ItemFrameEntityRendererMixin<T extends ItemFrameEntity> extends EntityRenderer<T> {
+	protected ItemFrameEntityRendererMixin(EntityRendererFactory.Context ctx) {
+		super(ctx);
+	}
+
 	@Redirect(method = "render(Lnet/minecraft/entity/decoration/ItemFrameEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(FFF)V", ordinal = 1))
 	private void disableInvisibleItemFrameOffset(MatrixStack matrixStack, float x, float y, float z) {
 		matrixStack.translate(x, y, 0.45F);
+	}
+
+	@Inject(method = "renderLabelIfPresent(Lnet/minecraft/entity/decoration/ItemFrameEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", at = @At("HEAD"), cancellable = true)
+	private void disableInvisibleItemFrameLabel(T itemFrameEntity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, float f, CallbackInfo ci) {
+		if (itemFrameEntity.isInvisible()) ci.cancel();
 	}
 }
